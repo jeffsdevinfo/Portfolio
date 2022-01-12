8e53fc9ea5ad96e92677039f260793e890576459
Jeff <jeffsaieinfo@gmail.com>
2022-01-12 12:57:16 -0800

    Input System WIP

---
+++ Portfolio1/Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTankHandling(InputValue value)
+    {
+        Debug.Log($"Outputing input value {value.Get<Vector2>()}");
+    }
+}
