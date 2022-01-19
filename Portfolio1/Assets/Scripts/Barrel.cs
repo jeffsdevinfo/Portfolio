48153f889ddc1b0c5657b66682b7b4381f7b2ac5
nullu5er <jeffsaieinfo@gmail.com>
2022-01-18 23:03:10 -0800

    turret and barrel wip
    turret moving, barrel pitches but gets scaled unintentionally

---
+++ Portfolio1/Assets/Scripts/Barrel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Barrel : MonoBehaviour
+{
+    [SerializeField] GameObject barrel;
+    [SerializeField] GameObject barrelPivot;
+    [SerializeField] float pitchSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(bPitchActive)
+        {
+            barrelPivot.transform.Rotate(Vector3.right, 1 * pitchSpeed * barrelRotRate);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool bPitchActive = false;
+    float barrelRotRate = 0.0f;
+    public void OnBarrelHandling(InputValue value)
+    {
+        Debug.Log($"Ouputing barrel handling value {value.Get<float>()}");
+        barrelRotRate = value.Get<float>();
+        if (Mathf.Abs(barrelRotRate) > Mathf.Epsilon)
+        {
+            bPitchActive = true;
+        }
+        else
+        {
+            barrelRotRate = 0.0f;
+            bPitchActive = false;
+        }
+    }
+}
--- Portfolio1/Assets/Scripts/Turret.cs
+++ Portfolio1/Assets/Scripts/Turret.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class Turret : MonoBehaviour
 {
+    [SerializeField] GameObject turret;
+    [SerializeField] float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,35 @@ public class Turret : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        if(bRotateActive)
+        {
+            turret.transform.Rotate(Vector3.up, 1 * rotationSpeed * turretRotDir);
+        }
+    }
+
     public void OnTankHandling(InputValue value)
     {
         Debug.Log($"Outputing input value {value.Get<Vector2>()}");
     }
+
+
+    bool bRotateActive = false;
+    float turretRotDir = 0.0f;
+    public void OnTurretHandling(InputValue value)
+    {
+        Debug.Log($"Ouputing turret handling value {value.Get<float>()}");
+        turretRotDir = value.Get<float>();
+        if(Mathf.Abs(turretRotDir) > Mathf.Epsilon)
+        {
+            bRotateActive = true;          
+        }
+        else
+        {
+            turretRotDir = 0.0f;
+            bRotateActive = false;
+        }
+    }
+    
 }
